ba7583fc5d4635920b9d4357a76b35eb7ded3e48
shibataka000 <chise.alter.pasta@gmail.com>
2018-03-02 20:53:43 +0900

    Modify ssh key name.

--- windows_server_2016/get-password-data.sh
+++ windows_server_2016/get-password-data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 INSTANCE_ID=`terraform output instance_id`
-SSH_KEY=$HOME/.ssh/ec2_default.pem
+SSH_KEY=$HOME/.ssh/aws_default
 aws ec2 get-password-data --instance-id $INSTANCE_ID --priv-launch-key $SSH_KEY
